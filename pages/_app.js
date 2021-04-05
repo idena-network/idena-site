12aa33f05e95df0efb0d40f8daac9863d3e4e304
videnaio <v@idena.io>
2021-04-05 16:06:49 +0500

    Fix google tag manager code

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import {useEffect} from 'react'
 
 function MyApp({Component, pageProps}) {
   useEffect(() => {
-    ReactGA.initialize('UA-000000-01')
+    ReactGA.initialize('UA-139651161-1')
     ReactGA.pageview(window.location.pathname + window.location.search)
   }, [])
 
