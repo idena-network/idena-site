6acbbd2859609b896f56d76b20b52c83d3c6e77c
ridenaio <r@idena.io>
2021-07-10 21:30:09 +0500

    Pass env var to client

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withSass = require('@zeit/next-sass')
 const withFonts = require('nextjs-fonts')
 const {i18n} = require('./next-i18next.config')
 
-module.exports = withSass(withFonts({i18n}))
+module.exports = withSass(
+  withFonts({
+    i18n,
+    env: {
+      TWITTER_MINIMUM_SUBS_COUNT: process.env.TWITTER_MINIMUM_SUBS_COUNT,
+    },
+  })
+)
