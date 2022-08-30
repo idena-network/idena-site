9bcbd65916d43986c70f45424bd0036d1fc2f4e8
ridenaio <r@idena.io>
2022-08-30 21:20:03 +0500

    Update old packages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
-const withSass = require('@zeit/next-sass')
 const withFonts = require('nextjs-fonts')
+const path = require('path')
 const {i18n} = require('./next-i18next.config')
 
-module.exports = withSass(
-  withFonts({
-    i18n,
-    env: {
-      TWITTER_MINIMUM_SUBS_COUNT: process.env.TWITTER_MINIMUM_SUBS_COUNT,
-    },
-  })
-)
+module.exports = withFonts({
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  i18n,
+  env: {
+    TWITTER_MINIMUM_SUBS_COUNT: process.env.TWITTER_MINIMUM_SUBS_COUNT,
+  },
+})
